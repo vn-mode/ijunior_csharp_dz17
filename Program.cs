4f5c33456789e7934011b426224740e177428940
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-05-25 21:35:33 +0300

    dz17
    dz17

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace vn_mode_csharp_dz17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNumber;
+            int degree = 1;
+            int dynamicNumber;
+            int sourceNumber = 2;
+
+            dynamicNumber = sourceNumber;
+            Random rand = new Random();
+            randomNumber = rand.Next(1, 100);
+
+            for (int i = 1; dynamicNumber < randomNumber; i = i * sourceNumber)
+            {
+                dynamicNumber *= sourceNumber;
+                degree++;
+            }
+
+            if (randomNumber == dynamicNumber)
+            {
+                dynamicNumber *= sourceNumber;
+                degree++;
+            }
+            
+            Console.WriteLine($"Исходное число: {randomNumber}\n{sourceNumber} в степени: {degree}\nРезультат возведения в степень: {dynamicNumber}");
+
+        }
+    }
+}
